573289f0226e274e14582b0fd9e5b34e0bd0949c
Hannes Preishuber <hannesp@ppedv.de>
2019-04-17 13:33:05 +0200

    Northwind Entity Framework

---
+++ WebformsWien2019/Eftest.aspx
@@ -0,0 +1,5 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Eftest.aspx.cs" Inherits="WebformsWien2019.Eftest" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="SchlussHTML" runat="server">
+</asp:Content>
---
+++ WebformsWien2019/Eftest.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using WebformsWien2019.Models;
+
+namespace WebformsWien2019
+{
+    public partial class Eftest : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var ef = new Model1();
+            var anzahl=ef.Customers.First().Orders.Count();
+
+
+        }
+    }
+}
