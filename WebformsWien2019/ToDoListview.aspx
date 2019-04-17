604c798c6e0cf500b77010d967661a79b55d1dc0
Hannes Preishuber <hannesp@ppedv.de>
2019-04-17 11:35:46 +0200

    todo Delete Item

--- WebformsWien2019/ToDoListview.aspx
+++ WebformsWien2019/ToDoListview.aspx
@@ -1,5 +1,30 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="ToDoListview.aspx.cs" Inherits="WebformsWien2019.ToDoListview" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+        <div class="row">
+        <div class="col-5">
+            <ul class="list-group">
+               
+                <asp:Listview ID="TodoListview" runat="server" DataKeyNames="Id"
+                    EnableViewState="false"
+                    OnItemCommand="TodoListview_ItemCommand1"
+                    DeleteMethod="TodoListview_DeleteItem"
+                    ItemType="WebformsWien2019.Models.ToDo" SelectMethod="TodoListView_GetData">
+                    <ItemTemplate>
+                          <li class="list-group-item">
+                             
+                              <asp:Button ID="Button1" runat="server" Text="erledigen" 
+                                  CommandName="erledigen" CommandArgument="<%#Item.Id %>" />
+                              <%#Item.Text%> 
+                              
+                          </li>
+                    </ItemTemplate>
+                </asp:Listview>
+ 
+            </ul>
+        </div>
+    </div>
+
+
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="SchlussHTML" runat="server">
 </asp:Content>
--- WebformsWien2019/ToDoListview.aspx.cs
+++ WebformsWien2019/ToDoListview.aspx.cs
@@ -4,12 +4,62 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using WebformsWien2019.Models;
 
 namespace WebformsWien2019
 {
     public partial class ToDoListview : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        public List<ToDo> ToDoListview_GetData()
+        {
+            var liste = new List<ToDo>();
+
+            liste.Add(new ToDo() { Id = 1, Text = "Pause machen" });
+            liste.Add(new ToDo() { Id = 2, Text = "Kaffe trinken" });
+            liste.Add(new ToDo() { Id = 3, Text = "bla blah" });
+            liste.Add(new ToDo() { Id = 4, Text = "sdfdfgdfg" });
+            liste.Add(new ToDo() { Id = 5, Text = "fgfghfghfgh" });
+
+            return liste;
+        }
+
+        protected void ToDoListview_ItemCommand(object source, RepeaterCommandEventArgs e)
+        {
+       
+        }
+
+        protected void TodoListview_ItemCommand1(object sender, ListViewCommandEventArgs e)
+        {
+            if (e.CommandName == "erledigen")
+            {
+                int id = Convert.ToInt32(e.CommandArgument);
+                int i = 0;
+                foreach (DataKey item in TodoListview.DataKeys)
+                {
+                    
+                    if (Convert.ToInt32(item.Value) == id)
+                    {
+                        TodoListview.DeleteItem(i);
+                    //    TodoListview.Items.RemoveAt(i);
+                       
+                        break;
+                    }
+                    i++;
+                }
+
+
+
+
+
+            }
+        }
+
+        // Der Name des ID-Parameters sollte dem für das Steuerelement festgelegten DataKeyNames-Wert entsprechen.
+        public void TodoListview_DeleteItem(int id)
         {
 
         }
