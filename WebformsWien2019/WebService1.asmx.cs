701b947f707b959a415b3f411a406be8a78be669
Hannes Preishuber <hannesp@ppedv.de>
2019-04-18 13:45:33 +0200

    ASMX Webservice mit JSON

---
+++ WebformsWien2019/WebService1.asmx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Script.Services;
+using System.Web.Services;
+
+namespace WebformsWien2019
+{
+    /// <summary>
+    /// Zusammenfassungsbeschreibung für WebService1
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [ScriptService] //nötig für JSON response
+    [System.ComponentModel.ToolboxItem(false)]
+    // Wenn der Aufruf dieses Webdiensts aus einem Skript zulässig sein soll, heben Sie mithilfe von ASP.NET AJAX die Kommentarmarkierung für die folgende Zeile auf. 
+    // [System.Web.Script.Services.ScriptService]
+    public class WebService1 : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        [ScriptMethod(UseHttpGet =true)]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+    }
+}
