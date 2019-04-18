8241048c67f10c064e01c5ba23b2a15830b9b0c1
Hannes Preishuber <hannesp@ppedv.de>
2019-04-18 10:57:21 +0200

    Ajax Updatepanel mit Timer

---
+++ WebformsWien2019/Webformajayx.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Webformajayx.aspx.cs" Inherits="WebformsWien2019.Webformajayx" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <asp:UpdatePanel ID="UpdatePanel1" runat="server" UpdateMode="Conditional">
+
+        <ContentTemplate>
+            <asp:Label runat="server" Text="Label" id="Label1"></asp:Label>  
+    
+        </ContentTemplate>
+        <Triggers>
+            <asp:AsyncPostBackTrigger ControlID="Timer1" />
+        </Triggers>
+    </asp:UpdatePanel>
+          <asp:Timer ID="Timer1" runat="server" Interval="1000"></asp:Timer>
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="SchlussHTML" runat="server">
+</asp:Content>
---
+++ WebformsWien2019/Webformajayx.aspx.designer.cs
@@ -0,0 +1,42 @@
+﻿//------------------------------------------------------------------------------
+// <automatisch generiert>
+//     Dieser Code wurde von einem Tool generiert.
+//
+//     Änderungen an dieser Datei können fehlerhaftes Verhalten verursachen und gehen verloren, wenn
+//     der Code neu generiert wird.
+// </automatisch generiert>
+//------------------------------------------------------------------------------
+
+namespace WebformsWien2019 {
+    
+    
+    public partial class Webformajayx {
+        
+        /// <summary>
+        /// UpdatePanel1-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Zum Ändern Felddeklaration aus der Designerdatei in eine Code-Behind-Datei verschieben.
+        /// </remarks>
+        protected global::System.Web.UI.UpdatePanel UpdatePanel1;
+        
+        /// <summary>
+        /// Label1-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Zum Ändern Felddeklaration aus der Designerdatei in eine Code-Behind-Datei verschieben.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label1;
+        
+        /// <summary>
+        /// Timer1-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Zum Ändern Felddeklaration aus der Designerdatei in eine Code-Behind-Datei verschieben.
+        /// </remarks>
+        protected global::System.Web.UI.Timer Timer1;
+    }
+}
